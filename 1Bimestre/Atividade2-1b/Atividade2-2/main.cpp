613aba7f0dd897cfaa0b899e9cc654fcbcc9e44e
Cassio Mariano <cassiomariano774@gmail.com>
2023-04-17 21:52:33 -0300

    identação dos codigos corrigida

--- 1Bimestre/Atividade2-1b/Atividade2-2/main.cpp
+++ 1Bimestre/Atividade2-1b/Atividade2-2/main.cpp
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 #include <iostream>
-#include <gl/glut.h>
+#include <GL/glut.h>
 #include <list>
 #include <math.h>
 #include <cmath>
@@ -103,90 +102,3 @@ int main(int argc, char **argv)
     glutMainLoop();
     return 0;
 }
-=======
-#include <iostream>
-#include <GL/glut.h>
-#include <list> 
-#include <math.h>
-#include <cmath>
-
-using namespace std;
-
-struct PONTO {int x; int y;}; //Registro "PONTO", onde ficará armazenado as coordenadas do ponto clicado.
-
-list<PONTO> pontos; //Lista do tipo "Ponto", onde será armazenado todos os pontos clicados.
-
-double funcaoEuclidiana(int x1, int y1, int x2, int y2){ //Função que determina a menor distância entre dois pontos.
-    return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2)); //formula da distância euclidiana
-}
-
-void pegarPontoProximo(int x, int y){
-    auto menorDistancia = INFINITY; //Atribui a menor distancia ao infinito na primeira inicialização.
-    auto pontoProximo = pontos.begin(); // Toma como base o primeiro ponto como o mais proximo.
-    for(auto ponto = pontos.begin(); ponto != pontos.end(); ponto++){ //Percorrendo a lista
-        double temp = funcaoEuclidiana(x, y, ponto->x, ponto->y); // atribui o retorno da função euclidiana
-        if(temp < menorDistancia){ // se a distancia do ponto atual for menor
-            menorDistancia = temp; // a menor distancia é atribuida a esse ponto atual
-            pontoProximo = ponto; // o ponto mais proximo é atualizado com o ponto atual
-        }
-    }
-    pontos.erase(pontoProximo); //faz a exclusão do ponto mais próximo.
-    pontos.push_back({pontoProximo->x, pontoProximo->y}); //Adiciona o ponto próximo como o ultimo da lista.
-}
-
-void mouse(int botao, int estado, int x, int y) {
-    if (botao == GLUT_LEFT_BUTTON && estado == GLUT_DOWN){ //Se o clique foi no botão esquerdo e o estado do botão está para baixo
-        pontos.push_back({x, y}); //Adiciona o ponto na lista passando as coordenadas xy.
-    } else if (botao == GLUT_RIGHT_BUTTON && estado == GLUT_DOWN){ //Se o clique foi no botão direito e o estado do botão está para baixo
-        pegarPontoProximo(x, y);//Chama a função de pegar o ponto mais proximo
-    } else {
-        return;
-    }
-    glutPostRedisplay();
-}
-
-void display() {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_POINTS); //GL_POINTS para desenhar pontos.
-    glColor3f(1.0, 1.0, 1.0); // Define cor dos pontos para branco
-    for (auto ponto = pontos.begin();  ponto != pontos.end(); ponto++){ //Percorrendo os pontos da lista
-        glVertex2f((double)ponto->x, (double)ponto->y); //Desenhando os pontos na tela
-    }
-    glEnd();
-    glFlush();
-}
-
-void keyboard(int key, int x, int y){
-    auto ultimoPonto = pontos.back();// pega o ultimo ponto
-    switch (key){
-    case GLUT_KEY_UP:
-        ultimoPonto.y -= 10; //Movendo o ponto
-        break;
-    case GLUT_KEY_DOWN:
-        ultimoPonto.y += 10; 
-        break;
-    case GLUT_KEY_RIGHT:
-        ultimoPonto.x += 10;
-        break;
-    case GLUT_KEY_LEFT:
-        ultimoPonto.x -= 10;
-        break;
-    }
-    pontos.back() = ultimoPonto;//Atualiza o ponto com as novas coordenadas
-    glutPostRedisplay();
-}
-
-int main(int argc, char **argv) {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(800, 600);
-    glutCreateWindow("Atividade 1 - Linhas");
-    gluOrtho2D(0, 800, 600, 0);
-    glPointSize(10.0); // Define o tamanho do ponto
-    glutMouseFunc(mouse); //Função do Mouse
-    glutSpecialFunc(keyboard);//Função do teclado
-    glutDisplayFunc(display);
-    glutMainLoop();
-    return 0;
-}
->>>>>>> b42cdd9815f585d2478c4a666560fad0ae25454c
--- 1Bimestre/Atividade3-1b/Atividade3-2/main.cpp
+++ 1Bimestre/Atividade3-1b/Atividade3-2/main.cpp
@@ -3,7 +3,8 @@
 #include <list>
 #include <GL/glut.h>
 
-struct Point {
+struct Point
+{
     float x;
     float y;
 };
@@ -12,39 +13,44 @@ std::list<Point> pontos;
 
 void openFile()
 {
-    std::ifstream inFile("C:/Users/Cassio/Desktop/dino.Dat");
-    if (!inFile) {
-        std::cerr << "Erro de arquivo! " << std::endl;
+    std::ifstream inFile("C:/Users/Cassio/Desktop/dino.Dat"); // Abrindo o arquivo dino.dat
+    if (!inFile)
+    {
+        std::cerr << "Erro de arquivo! " << std::endl; // Erro se o arquivo não existir.
         return;
     }
 
-    int numPolig, numVertices;
+    int numPolig, numVertices; // quantidade de poligonos e vertices
     float x, y;
-    inFile >> numPolig;
+    inFile >> numPolig; // pega o núcleo para saber a quantidade de poligonos
     std::list<Point> vertices;
-    for (int i = 0; i < numPolig; ++i) {
-        inFile >> numVertices;
-        for (int j = 0; j < numVertices; j++) {
-            inFile >> x >> y;
-            pontos.push_back({ x, y });
+    for (int i = 0; i < numPolig; ++i)
+    {                          // percorrendo todos os poligonos
+        inFile >> numVertices; // pegando os vertices
+        for (int j = 0; j < numVertices; j++)
+        {                             // percorrendo os vertices
+            inFile >> x >> y;         // colocando as coordenadas nas variaveis
+            pontos.push_back({x, y}); // adicionando na lista de pontos.
         }
     }
     inFile.close();
 }
 
-void display() {
+void display()
+{
     openFile();
     glClear(GL_COLOR_BUFFER_BIT);
     glColor3f(1.0, 1.0, 1.0);
     glBegin(GL_LINE_STRIP);
-    for (auto ponto = pontos.begin(); ponto != pontos.end(); ponto++) {
-        glVertex2f(ponto->x, ponto->y);
+    for (auto ponto = pontos.begin(); ponto != pontos.end(); ponto++)
+    {                                   // percorrendo a lista
+        glVertex2f(ponto->x, ponto->y); // desenhando as linhas
     }
     glEnd();
     glFlush();
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
