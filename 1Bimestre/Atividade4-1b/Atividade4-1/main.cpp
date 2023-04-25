36836c38a86d026744763c997bec9e6d637d8466
Cassio Mariano <cassiomariano774@gmail.com>
2023-04-25 11:41:17 -0300

    Código corrigido

--- 1Bimestre/Atividade4-1b/Atividade4-1/main.cpp
+++ 1Bimestre/Atividade4-1b/Atividade4-1/main.cpp
@@ -1,42 +1,55 @@
-#include <iostream>
-#include <cmath>
 #include <GL/glut.h>
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
 
-void display()
+int mouseX, mouseY;
+bool flag = false;
+int raio = 50;
+
+void display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
-
-    glBegin(GL_LINE_STRIP);
-
     glColor3f(1.0, 1.0, 1.0);
 
-    // Loop para desenhar a função
-    for (float x = -3.14; x <= 3.14; x += 0.1)
-    {
-        float y = sin(x); // Função senoidal f(x) = sen(x)
-        glVertex2f(x, y); // Desenha o ponto (x, y)
+    if (flag)
+    { // se o mouse foi clicado, inicia o desenho
+        glBegin(GL_LINE_STRIP);
+        glVertex2f(mouseX, -90 + mouseY); // Primeira linha da teardrop
+        double angulo = 0;
+        for (double i = 0; i <= 7000; i++)
+        {
+            glVertex2i(mouseX + raio * cos(angulo), mouseY + raio * sin(angulo)); // desenhando o circulo
+            angulo = i * (M_PI / 7000);                                           // faz um meio circulo, convertendo para radianos o angulo é 180 graus.
+        }
+
+        glVertex2f(mouseX, -90 + mouseY); // desenha a segunda linha da teardrop
+
+        glEnd();
     }
 
-    glEnd();
-
     glFlush();
+}
+
+void mouse(int button, int state, int x, int y)
+{
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+    {
+        mouseX = x;
+        mouseY = y;
+        flag = true;
+    }
+    glutPostRedisplay();
 }
 
 int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-
     glutInitWindowSize(500, 500);
-    glutCreateWindow("Atividade 04/2 | FunÃ§Ã£o : f(x) = sen(x)");
-
-    glClearColor(0.0, 0.0, 0.0, 0.0);
-
-    gluOrtho2D(-3.14, 3.14, -1.5, 1.5); // Configura a janela de visualização
-
+    glutCreateWindow("Atividade 04/1");
+    gluOrtho2D(0, 500, 500, 0);
     glutDisplayFunc(display);
-
+    glutMouseFunc(mouse);
     glutMainLoop();
-
-    return 0;
 }
